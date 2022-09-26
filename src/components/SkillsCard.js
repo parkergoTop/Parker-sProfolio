e85ac82a90a2781e20389f108a2fe28cc6be0614
Qingwen Xiao <parkerciao@gmail.com>
2022-09-26 11:40:47 +1000

    home page finished

--- src/components/SkillsCard.js
+++ src/components/SkillsCard.js
@@ -3,12 +3,15 @@ import React from "react"
 
 
 export default function skillsCard(props){
-
+  //props.skills[i].icon
     function setSkillCard(){
         const skills = []
         for(let i = 0; i < props.skills.length; i ++){
          
-           skills.push(<li> <img  className="skill-image" src={props.skills[i].icon} />{props.skills[i].skill}</li>)
+           skills.push(<li className="skill-element"> 
+                            <img   id={props.skills[i].icon==="" ? "no-skill-image" : "skill-image"}src={props.skills[i].icon} />
+                                                   {props.skills[i].skill}
+                       </li>)
         }
 
         return skills
@@ -16,7 +19,7 @@ export default function skillsCard(props){
        
       return(
         <div className="skill-card">
-               <h3 className ="skill-card-title"> Front-End </h3>
+               <h3 className ="skill-card-title"> {props.title} </h3>
 
                <ul className="skills-list"> 
                     {setSkillCard()}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,9 @@ import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
-import { Navigation } from "swiper";
+import "swiper/css/pagination";
+
+import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper";
 
 
 import GithubIcon from "@material-ui/icons/GitHub"
@@ -18,8 +20,15 @@ import cssIcon from "../images/css.png"
 import htmlIcon from "../images/html.png"
 import JSIcon from "../images/js.png"
 import reactIcon from "../images/react.png"
-
-
+import javaIcon from "../images/java.png"
+import gitIcon from "../images/git.png"
+import jspIcon from "../images/jsp.png"
+import springIcon from "../images/springboot.png"
+import sqlIcon from "../images/sql.png"
+import jiraIcon from "../images/jira.png"
+import androidIcon from "../images/android.png"
+import agileIcon from "../images/agile.png"
+import jenkinsIcon from "../images/jenkins.png"
 
 
 export default function Home(){
@@ -29,59 +38,73 @@ export default function Home(){
     const frontendSkills = {category: "Front End", skills : [
                                                               {icon: cssIcon, skill: "CSS (& SASS)"}, {icon: htmlIcon, skill: "HTML"}, {icon: JSIcon, skill: "JS (Valinna, ES6+)"} , {icon:reactIcon , skill: "React"}
                                                                   ]}
+    const backendSkills = {category: "Back End", skills : [
+                                                            {icon: javaIcon, skill: "Java"}, {icon: jspIcon, skill: "JSP"}, {icon:springIcon, skill: "Springboot"} , {icon: sqlIcon , skill: "SQL"}, {icon: androidIcon, skill: "Android Development"},
+                                                                        ]}
 
+     const tools = {category: "Tools & Platforms", skills : [
+                                                    {icon: agileIcon, skill: "Agile/Scrum"}, {icon: jiraIcon, skill: "JIRA/Confluence"}, {icon: gitIcon, skill: "Git/Github"} , {icon:jenkinsIcon , skill: "Jenkins"},
+                                                                                ]}
+
+    const cs = {category: "CS Essentials", skills : [
+                                                       {icon: "", skill: "Computor Netwroking HTTP/TCP"}, {icon: "", skill: "Data Structure & Algorithm"}, {icon: "", skill: "Unix/Linix"} , {icon:"", skill: "Data Analytics & Neural Network"},
+                                                                                                                ]}
     return <div className ="home-page"> 
          
          <hr/>
 
-         <div className="about">
-
-            <div className="left-col-about">
-            <img className="selfie" src= "#" />
-            <h2 className="name">Parker Ciao </h2>
-            </div >
+        <div className="about">
+           <h2 className="home-title"> About </h2>
+           <div className="about-content">
+              <div className="left-col-about">
+              <img className="selfie" src= "#" />
+              <h2 className="name">Parker Ciao </h2>
+              </div >
 
          
-             <div className="right-col-about">
-                <p> Software Engineer</p>
-                <p> Bachelor of Science in IT with Distinction, University of Technology Sydney</p>
-                <p> Was working for HSBC</p>
-                <p> Passionate Creator,  Quick Learner</p>
-                <div className="phone-detail">  <PhoneIcon/>   +61 0404842938 </div>
-                <div className="email-detail">  <EmailIcon/>  parkerciao@gmail.com </div>
-                <div className="github-detail">    <GithubIcon/> https://github.com/parkergoTop</div>
+               <div className="right-col-about">
+                  <p> Software Engineer</p>
+                  <p> Bachelor of Science in IT with Distinction, University of Technology Sydney</p>
+                  <p> Was working for HSBC</p>
+                  <p> Passionate Creator,  Quick Learner</p>
+                  <div className="phone-detail">  <PhoneIcon/>   +61 0404842938 </div>
+                  <div className="email-detail">  <EmailIcon/>  parkerciao@gmail.com </div>
+                  <div className="github-detail">    <GithubIcon/> https://github.com/parkergoTop</div>
              </div>
+        </div>
              
          </div>
 
          <hr/>
          <div className="skills"> 
-             
-             <Swiper navigation={true} modules={[Navigation]} >
+          <h2 className="home-title"> Skills </h2>
+         <Swiper  cssMode={true}  navigation={true} pagination={true} mousewheel={true} keyboard={true}
+               modules={[Navigation, Pagination, Mousewheel, Keyboard]}className="mySwiper" >
                 <SwiperSlide className="myswiper">
                     
                      <SkillsCard
                          key= {frontendSkills.category}
+                         title={frontendSkills.category}
                          skills={frontendSkills.skills}/>
 
                      <SkillsCard
-                         key= {frontendSkills.category}
-                         skills={frontendSkills.skills}/>
+                         key= {backendSkills.category}
+                         title={backendSkills.category}
+                         skills={backendSkills.skills}/>
 
                       <SkillsCard
-                         key= {frontendSkills.category}
-                         skills={frontendSkills.skills}/>
+                         key= {tools.category}
+                         title={tools.category}
+                         skills={tools.skills}/>
  
                 </SwiperSlide>
 
                 <SwiperSlide className="myswiper">
                      <SkillsCard
-                         key= {frontendSkills.category}
-                         skills={frontendSkills.skills}/>
+                         key= {cs.category}
+                         title={frontendSkills.category}
+                         skills={cs.skills}/>
 
-                     <SkillsCard
-                         key= {frontendSkills.category}
-                         skills={frontendSkills.skills}/>
                 </SwiperSlide>
                 
                 
