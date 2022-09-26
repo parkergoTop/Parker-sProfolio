c3b1147a9edb7be5682b395c49695cb9524479a8
Qingwen Xiao <parkerciao@gmail.com>
2022-09-26 16:33:35 +1000

    open window function aded

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import "./footer.css"
 import React from "react"
 
 import LinkedInIcon from "@material-ui/icons/LinkedIn"
-import  EmailIcon from "@material-ui/icons/Email"
+import  GitHub from "@material-ui/icons/GitHub"
 
 import InstagramIcon from "@material-ui/icons/Instagram"
 
@@ -11,9 +11,9 @@ export default function Footer(){
     return(
         <div className="footer">
           <div className="socialMedia"> 
-            <LinkedInIcon/>
-            <EmailIcon/>
-            <InstagramIcon/>
+            <LinkedInIcon  onClick = {() => window.open("https://www.linkedin.com/in/qingwen-xiao-031508234/")}/>
+            <GitHub  onClick = {() => window.open("https://github.com/parkergoTop")}/>
+            <InstagramIcon  onClick = {() => window.open("https://www.linkedin.com/in/qingwen-xiao-031508234/")}/>
           </div>
 
         </div>
